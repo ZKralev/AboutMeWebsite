f18d938183594af90648570e2f66a0d6708ebf3a
Zdravko Kralev <driftedmind@outlook.com>
2026-04-24 15:52:21 +0300

    Updates

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-const { override } = require('customize-cra');
-
-// No custom webpack plugins are required. CRA already handles copying
-// assets from `public/` to `build/` during production builds.
-module.exports = override();
